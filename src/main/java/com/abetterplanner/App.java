4760e8741fd13c4f690c901b082b7d194b330140
Benton Liu <bentonliu@bentonliu.com>
2018-11-04 20:06:25 -0800

    Fixed Spring dependencies Started Spring Framework Changed Spring properties to start on port 80 instead of 8080 Changed App.java to start Spring Framework

--- src/main/java/com/abetterplanner/App.java
+++ src/main/java/com/abetterplanner/App.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableAutoConfiguration
--- src/main/java/com/abetterplanner/controller/Controller.java
+++ src/main/java/com/abetterplanner/controller/Controller.java
@@ -1,13 +1,31 @@
 package com.abetterplanner.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RestController
 public class Controller {
 
-    public int add(int x, int y) {
-        return x + y;
+    int add(int x, int y){
+        return x+y;
     }
 
-    public String hello() {
-        String a = "Hello";
-        return a;
+    /**
+     * This is a simple example of how the HTTP API works.
+     * It returns a String "OK" in the HTTP response.
+     * To try it, run the web application locally,
+     * in your web browser, type the link:
+     * 	http://localhost:8080/cs480/ping
+     */
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    String healthCheck() {
+        return "ping";
     }
 }

c90b06daf54f7ad85e8451aa355d037a7d8bee72
eyMiko <ayymiko@gmail.com>
2018-11-27 09:31:08 -0800

    updated CourseList

--- src/main/resources/Front-end/src/containers/CourseList.js
+++ src/main/resources/Front-end/src/containers/CourseList.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react'
 import courses from '../course.json'
+import PropTypes from 'prop-types';
+import checkboxes from '../components/checkboxes';
+import Checkbox from '../components/CheckBox';
 
 class CourseList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checkedItems: new Map(),
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const item = e.target.name;
+    const isChecked = e.target.checked;
+    this.setState(prevState => ({ checkedItems: prevState.checkedItems.set(item, isChecked) }));
+  }
   render() {
     return (
-      <div>
-      {courses.map((item, index) => {
-        return <table>
-          <tr>
-          <td>{item.course_number}</td>
+      <React.Fragment> {
+        courses.map(item => (
+          <label key={item.key}>
+          <table>{item.course_number}
           <td>{item.class_number}</td>
-          <td>{item.building}</td>
-          <td>{item.time}</td>
           <td>{item.instructor}</td>
-          </tr>
+          <Checkbox name={item._id} checked={this.state.checkedItems.get(item._id)} onChange={this.handleChange} />
           </table>
-      })}
-      </div>
-    )
+          </label>
+        ))
+      }
+      </React.Fragment>
+    );
   }
 }
+
 export default CourseList

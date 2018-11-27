fa521d47f36bda3797c01ca7fc92e01c8719c142
eyMiko <ayymiko@gmail.com>
2018-11-27 04:04:06 -0800

    testing files

--- src/main/resources/Front-end/src/App.js
+++ src/main/resources/Front-end/src/App.js
@@ -1,15 +1,15 @@
 
 import React, { Component } from 'react';
 import { render } from "react-dom";
-import Registration from './containers/Registration';
-import Login from './containers/Login';
+import CourseList from './containers/CourseList'
+import courses from './course.json';
 
 class App extends Component {
   render() {
     return (
       <div className="col-md-6">
         <h1> Welcome to A Better Planner!  </h1>
-        <Registration />
+        <CourseList />
       </div>
     );
   }
